150ce1a3ffac6dbf183f43faabd99ec2710e0383
eseosaO <45127782+eseosaO@users.noreply.github.com>
2019-11-24 20:54:05 +0000

    A maths program file with common functions

---
+++ functionc.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+ 
+int main()
+{
+   int first, second, add, subtract, multiply;
+   float divide;
+ 
+   printf("Enter two integers\n");
+   scanf("%d%d", &first, &second);
+ 
+   add = first + second;
+   subtract = first - second;
+   multiply = first * second;
+   divide = first / (float)second;   //typecasting
+ 
+   printf("Sum of the numbers = %d\n", add);
+   printf("Difference of the numbers = %d\n", subtract);
+   printf("Multiplication of the numbers = %d\n", multiply);
+   printf("Division of the numbers = %.2f\n", divide);
+ 
+   return 0;
+}
